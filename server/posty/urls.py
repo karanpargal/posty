7a9b6b89d76788c6f156cabbd9620ea369474a07
Karan Pargal <karanpargal007@gmail.com>
2023-01-18 21:49:22 +0530

    add basic template editor

--- server/posty/urls.py
+++ server/posty/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include  # Ensure `include` is imported
-from posty.views import TemplateViewSet, Template_formatViewSet, Template_colorViewSet
+from django.urls import path, include
+from posty.views import TemplateViewSet, Template_formatViewSet, Template_colorViewSet, getImageURL, getMultipleTemplates, generateTemplates
 
 urlpatterns = [
     path('templates/', TemplateViewSet.as_view({'get': 'list', 'post': 'create'})),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('template_formats/<int:pk>/', Template_formatViewSet.as_view({'get': 'retrieve'})),
     path('template_colors/', Template_colorViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('template_colors/<int:pk>/', Template_colorViewSet.as_view({'get': 'retrieve'})),
+    path('getDownloadURL/<int:template_id>/<int:format_id>/<int:color_id>/', getImageURL.as_view()),
+    path('getMultipleTemplates/<str:format_value>/', getMultipleTemplates.as_view()), #currently not working
+    path('generateTemplates/<str:template_name>/', generateTemplates.as_view()),
 ]
--- server/posty/views.py
+++ server/posty/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from posty.models import Template, Template_format, Template_color
 from posty.serializers import TemplateSerializer, Template_formatSerializer, Template_colorSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from bs4 import BeautifulSoup as bs
+import os
+import re
+import imgkit
+from html2image import Html2Image
 # Create your views here.
 class TemplateViewSet(viewsets.ModelViewSet):
     queryset = Template.objects.all()
@@ -14,3 +20,46 @@ class Template_formatViewSet(viewsets.ModelViewSet):
 class Template_colorViewSet(viewsets.ModelViewSet):
     queryset = Template_color.objects.all()
     serializer_class = Template_colorSerializer
+
+class getImageURL(APIView):
+    def get(self, request, template_id, format_id, color_id, format=None):
+        try:
+            template = Template_color.objects.get(template_id=template_id, format_id=format_id, color_id=color_id)
+            read_serializer = Template_colorSerializer(template)
+            return Response(read_serializer.data)
+        except:
+            return Response(status=404)
+    
+class getMultipleTemplates(APIView):
+    def get(self, request,format_value, format=None):
+        try:
+            template = Template_format.objects.filter(format = format_value)
+            read_serializer = Template_formatSerializer(template)
+            return Response(read_serializer.data)
+        except:
+            return Response(status=404)
+
+class generateTemplates(APIView):
+    def get(self, request, template_name, format=None):
+        try:
+            data = request.query_params
+            base = os.path.dirname(os.path.abspath(__file__))
+            html = open(os.path.join(base, template_name+".html"))
+            soup = bs(html, 'html.parser')
+            
+            old_Title = soup.find("p", {"id": "title"})
+            new_Title = old_Title.find(text=re.compile('Enter Title')).replace_with(data['title'])
+            old_Punchline = soup.find("p", {"id": "punchline"})
+            new_Punchline = old_Punchline.find(text=re.compile('Enter Punchline')).replace_with(data['punchline'])
+            old_description = soup.find("p", {"id": "description"})
+            new_description = old_description.find(text=re.compile('Enter description')).replace_with(data['description'])
+
+            with open(template_name+".html", "wb") as f_output:
+                f_output.write(soup.prettify("utf-8"))
+
+            hti = Html2Image()
+            hti.screenshot(html_file=template_name+".html", save_as=template_name+".jpg")
+
+            return Response(data)
+        except:
+            return Response(status=404)
