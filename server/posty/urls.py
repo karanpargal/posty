d54ddeb9d88aec3589a6b546e8089e6c9540e47c
Karan Pargal <karanpargal007@gmail.com>
2023-01-21 12:38:31 +0530

    add s3 and unsplash api functions

--- server/posty/urls.py
+++ server/posty/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
-from posty.views import TemplateViewSet, Template_formatViewSet, Template_colorViewSet, getImageURL, getMultipleTemplates, generateTemplates
+from posty.views import TemplateViewSet, Template_formatViewSet, Template_colorViewSet, getImageURL, getMultipleTemplates, generateTemplates, uploadTemplateToS3
 
 urlpatterns = [
     path('templates/', TemplateViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('templates/<int:pk>/', TemplateViewSet.as_view({'get': 'retrieve'})),
-    path('template_formats/', Template_formatViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('template_formats/<int:pk>/', Template_formatViewSet.as_view({'get': 'retrieve'})),
-    path('template_colors/', Template_colorViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('template_colors/<int:pk>/', Template_colorViewSet.as_view({'get': 'retrieve'})),
+    path('templateFormats/', Template_formatViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('templateFormats/<int:pk>/', Template_formatViewSet.as_view({'get': 'retrieve'})),
+    path('templateColors/', Template_colorViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('templateColors/<int:pk>/', Template_colorViewSet.as_view({'get': 'retrieve'})),
     path('getDownloadURL/<int:template_id>/<int:format_id>/<int:color_id>/', getImageURL.as_view()),
-    path('getMultipleTemplates/<str:format_value>/', getMultipleTemplates.as_view()), #currently not working
+    path('getMultipleTemplates/<str:format_value>/', getMultipleTemplates.as_view()),
     path('generateTemplates/<str:template_name>/', generateTemplates.as_view()),
+    path('fetchTemplateFromS3/<str:template_name>/', fetchTemplateFromS3.as_view()),
+    path('uploadTemplateToS3/<str:template_name>/', uploadTemplateToS3.as_view()),
 ]
--- server/posty/views.py
+++ server/posty/views.py
@@ -6,9 +6,12 @@ from rest_framework.response import Response
 from bs4 import BeautifulSoup as bs
 import os
 import re
-import imgkit
+import boto3
 from html2image import Html2Image
-# Create your views here.
+import requests
+from django.conf import settings
+
+
 class TemplateViewSet(viewsets.ModelViewSet):
     queryset = Template.objects.all()
     serializer_class = TemplateSerializer
@@ -34,7 +37,7 @@ class getMultipleTemplates(APIView):
     def get(self, request,format_value, format=None):
         try:
             template = Template_format.objects.filter(format = format_value)
-            read_serializer = Template_formatSerializer(template)
+            read_serializer = Template_formatSerializer(template, many=True)
             return Response(read_serializer.data)
         except:
             return Response(status=404)
@@ -42,7 +45,12 @@ class getMultipleTemplates(APIView):
 class generateTemplates(APIView):
     def get(self, request, template_name, format=None):
         try:
+            
             data = request.query_params
+            bucket_name = "posty-templates"
+            fetchTemplateFromS3(bucket_name,template_name) #Give a bucket name and template name
+
+            #To generate custom text on the template
             base = os.path.dirname(os.path.abspath(__file__))
             html = open(os.path.join(base, template_name+".html"))
             soup = bs(html, 'html.parser')
@@ -63,3 +71,42 @@ class generateTemplates(APIView):
             return Response(data)
         except:
             return Response(status=404)
+
+def fetchTemplateFromS3(self, request, bucket_name, file_name, format=None):
+    session = boto3.Session(
+         aws_access_key_id='ACCESS_KEY',
+        aws_secret_access_key='SECRET_KEY',
+    )
+    s3 = session.client('s3')
+    s3 = boto3.client('s3')
+    try:
+        s3.download_file(bucket_name, file_name, '/path/to/local/file') #Recheck the path
+        print(f'Successfully downloaded {file_name} from {bucket_name}')
+    except:
+        print(f'Error downloading {file_name} from {bucket_name}')
+
+
+def uploadTemplateToS3(self, request, bucket_name, file_name, format=None):
+    session = boto3.Session(
+        aws_access_key_id='ACCESS_KEY',
+        aws_secret_access_key=' SECRET_KEY',
+    )
+    s3 = session.client('s3')
+    s3 = boto3.client('s3')
+    try:
+        s3.upload_file(f'/path/to/local/file', bucket_name, file_name) #Recheck the path
+        print(f'Successfully uploaded {file_name} to {bucket_name}')
+        url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+        return url
+    except:
+        print(f'Error uploading {file_name} to {bucket_name}')
+
+def fetchRandomImage():
+    headers = {'Authorization': f'Client-ID {settings.UNSPLASH_API_KEY}'}
+    url = 'https://api.unsplash.com/photos/random'
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()['urls']['regular']
+    except:
+        print('Error fetching image')
