7290dd960284a39a4e80362e6a62578bd9489a53
chinmayrelkar <chinmay@cohesive.so>
2023-02-03 13:39:49 +0530

    adds docker compose and, bug fix in middleware

--- server/server/settings.py
+++ server/server/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'cohesive_marketplace_django.auth.AuthMiddleware',
+    'cohesive_django.auth.AuthMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
