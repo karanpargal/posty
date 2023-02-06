0be3932e7e27245de9add3aaf7195fad2bdb9211
Karan Pargal <karanpargal007@gmail.com>
2023-02-07 03:22:53 +0530

    fix body input

--- client/src/components/InputScreen/InputScreen.js
+++ client/src/components/InputScreen/InputScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Axios } from '../../scripts/sdk-client';
+import { Axios } from "../../scripts/sdk-client";
 import Navbar from "../Navbar/Navbar";
 
 const InputScreen = () => {
@@ -71,9 +71,7 @@ const InputScreen = () => {
 
   const handleClickHeadline = () => {
     const prompt = document.getElementById("headline").value;
-    Axios.get(
-      `${URL}api/rephrasePrompt/?prompt=${prompt}`
-    )
+    Axios.get(`${URL}api/rephrasePrompt/?prompt=${prompt}`)
       .then((response) => response.data)
       .then((data) => {
         document.getElementById("headline").value = data.message;
@@ -82,9 +80,7 @@ const InputScreen = () => {
 
   const handleClickBody = () => {
     const prompt = document.getElementById("body").value;
-    Axios.get(
-      `${URL}api/rephrasePrompt/?prompt=${prompt}`
-    )
+    Axios.get(`${URL}api/rephrasePrompt/?prompt=${prompt}`)
       .then((response) => response.data)
       .then((data) => {
         document.getElementById("body").value = data.message;
@@ -101,8 +97,8 @@ const InputScreen = () => {
     console.log(url);
     Axios.get({
       url: url,
-      method: 'GET',
-      responseType: 'blob',
+      method: "GET",
+      responseType: "blob",
     })
       .then((response) => response.data)
       .then((blob) => {
@@ -162,7 +158,10 @@ const InputScreen = () => {
           <h1 className="font-semibold text-[36px] mb-3.5 mt-4">Edit Input</h1>
           <div className="">
             <h1 className="mb-2.5 text-[20px]">Social Media Platform</h1>
-            <select id="Platform" className="rounded-lg w-96 border-2 border-site-purple py-2 px-1 font-normal text-base mb-4">
+            <select
+              id="Platform"
+              className="rounded-lg w-96 border-2 border-site-purple py-2 px-1 font-normal text-base mb-4"
+            >
               <option selected>Choose a platform</option>
               <option value="Instagram">Instagram</option>
               <option value="Youtube">YouTube</option>
@@ -177,9 +176,9 @@ const InputScreen = () => {
               onChange={handleSetFormat}
             >
               <option selected>Choose a dimension</option>
-              <option value="1080x1080">1080x1080</option>
-              <option value="1920x1080">1920x1080</option>
-              <option value="1080x1920">1080x1920</option>
+              <option value="1080x1080">1:1</option>
+              <option value="1920x1080">16:9</option>
+              <option value="1080x1920">9:16</option>
             </select>
           </div>
           <div className="mb-4">
@@ -193,8 +192,7 @@ const InputScreen = () => {
           </div>
           <div className="mb-4">
             <h1 className="mb-2.5 text-[20px]">
-              Heading{" "}
-              <span className="text-gray-500 text-sm">(optional)</span>
+              Heading <span className="text-gray-500 text-sm">(optional)</span>
             </h1>
             <div className="flex w-96 h-14">
               <input
@@ -215,23 +213,23 @@ const InputScreen = () => {
           </div>
           <div className="mb-4">
             <h1 className="mb-2.5  text-[20px]">
-              Body{" "}
-              <span className="text-gray-500 text-sm">(optional)</span>
+              Body <span className="text-gray-500 text-sm">(optional)</span>
             </h1>
-            <div className="flex w-96 h-14">
-              <input
+            <div className="w-96">
+              <textarea
                 type="text"
-                className="border w-96 font-normal rounded-l-lg placeholder:pl-1 placeholder:text-[16px]"
+                class="w-96 p-2.5 resize-none border rounded-lg mb-1.5"
                 id="body"
+                rows="5"
                 placeholder="Eg. Sale! Sale! Sale!"
                 onChange={handleSetBody}
               />
               <button
                 type="button"
-                class="text-white bg-site-purple focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 focus:outline-none rounded-r-lg"
+                class="text-white w-96 bg-site-purple focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 focus:outline-none rounded-lg"
                 onClick={handleClickBody}
               >
-                Rephrase
+                Rephrase Body
               </button>
             </div>
           </div>
@@ -240,8 +238,7 @@ const InputScreen = () => {
               class="block mb-2.5 font-medium text-gray-900 text-[20px]"
               for="file_input"
             >
-              Logo{" "}
-              <span className="text-gray-500 text-sm">(optional)</span>
+              Logo <span className="text-gray-500 text-sm">(optional)</span>
             </label>
             <input
               className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 file:bg-site-purple file:text-white file:py-3 file:border-0"
@@ -286,37 +283,39 @@ const InputScreen = () => {
             <div className="grid grid-cols-3 gap-y-5 gap-x-6 mx-6">
               {templateURLs.map((url, index) => (
                 <div>
-                  <h1 className="font-semibold text-3xl my-4">Generated Templates</h1>
-                <div className="group relative ">
-                  <img
-                    src={url + `?${timestamp}`}
-                    alt="Template Generated"
-                    className="object-cover max-w-full max-h-full rounded-lg  group-hover:opacity-60 "
-                  />
-                  <svg
-                    class="absolute z-0 top-36 left-24 text-white opacity-0 group-hover:opacity-100 group-hover:z-2"
-                    onClick={() => {
-                      handleOpenImage(url, index);
-                    }}
-                  >
-                    <path
-                      d="M26.424 5.39545C30.7793 5.38097 35.0503 6.59583 38.7459 8.90037C42.4415 11.2049 45.4118 14.5056 47.3153 18.4229C43.4071 26.4052 35.4248 31.4503 26.424 31.4503C17.4232 31.4503 9.44098 26.4052 5.53274 18.4229C7.43621 14.5056 10.4065 11.2049 14.1021 8.90037C17.7978 6.59583 22.0687 5.38097 26.424 5.39545ZM26.424 0.658203C14.5809 0.658203 4.46686 8.02463 0.369141 18.4229C4.46686 28.8212 14.5809 36.1876 26.424 36.1876C38.2671 36.1876 48.3812 28.8212 52.4789 18.4229C48.3812 8.02463 38.2671 0.658203 26.424 0.658203ZM26.424 12.5013C27.9945 12.5013 29.5007 13.1252 30.6112 14.2357C31.7217 15.3462 32.3456 16.8524 32.3456 18.4229C32.3456 19.9934 31.7217 21.4996 30.6112 22.6101C29.5007 23.7206 27.9945 24.3445 26.424 24.3445C24.8535 24.3445 23.3473 23.7206 22.2368 22.6101C21.1263 21.4996 20.5025 19.9934 20.5025 18.4229C20.5025 16.8524 21.1263 15.3462 22.2368 14.2357C23.3473 13.1252 24.8535 12.5013 26.424 12.5013ZM26.424 7.76408C20.5498 7.76408 15.7652 12.5487 15.7652 18.4229C15.7652 24.2971 20.5498 29.0817 26.424 29.0817C32.2982 29.0817 37.0828 24.2971 37.0828 18.4229C37.0828 12.5487 32.2982 7.76408 26.424 7.76408Z"
-                      fill="white"
+                  <h1 className="font-semibold text-3xl my-4">
+                    Generated Templates
+                  </h1>
+                  <div className="group relative ">
+                    <img
+                      src={url + `?${timestamp}`}
+                      alt="Template Generated"
+                      className="object-cover max-w-full max-h-full rounded-lg  group-hover:opacity-60 "
                     />
-                  </svg>
-                  <svg
-                    class="absolute z-0 top-36 left-48 text-white opacity-0 group-hover:opacity-100 group-hover:z-2"
-                    onClick={() => {
-                      handleDownloadImage(url, index);
-                    }}
-                  >
-                    <path
-                      d="M5.3642 38.3726C4.06146 38.3726 2.94663 37.9091 2.0197 36.9822C1.0912 36.0537 0.626953 34.9381 0.626953 33.6354V26.5295H5.3642V33.6354H33.7877V26.5295H38.525V33.6354C38.525 34.9381 38.0615 36.0537 37.1346 36.9822C36.2061 37.9091 35.0904 38.3726 33.7877 38.3726H5.3642ZM19.576 28.8981L7.73283 17.055L11.0489 13.6205L17.2073 19.7789V0.474609H21.9446V19.7789L28.103 13.6205L31.4191 17.055L19.576 28.8981Z"
-                      fill="white"
-                    />
-                  </svg>
+                    <svg
+                      class="absolute z-0 top-36 left-24 text-white opacity-0 group-hover:opacity-100 group-hover:z-2"
+                      onClick={() => {
+                        handleOpenImage(url, index);
+                      }}
+                    >
+                      <path
+                        d="M26.424 5.39545C30.7793 5.38097 35.0503 6.59583 38.7459 8.90037C42.4415 11.2049 45.4118 14.5056 47.3153 18.4229C43.4071 26.4052 35.4248 31.4503 26.424 31.4503C17.4232 31.4503 9.44098 26.4052 5.53274 18.4229C7.43621 14.5056 10.4065 11.2049 14.1021 8.90037C17.7978 6.59583 22.0687 5.38097 26.424 5.39545ZM26.424 0.658203C14.5809 0.658203 4.46686 8.02463 0.369141 18.4229C4.46686 28.8212 14.5809 36.1876 26.424 36.1876C38.2671 36.1876 48.3812 28.8212 52.4789 18.4229C48.3812 8.02463 38.2671 0.658203 26.424 0.658203ZM26.424 12.5013C27.9945 12.5013 29.5007 13.1252 30.6112 14.2357C31.7217 15.3462 32.3456 16.8524 32.3456 18.4229C32.3456 19.9934 31.7217 21.4996 30.6112 22.6101C29.5007 23.7206 27.9945 24.3445 26.424 24.3445C24.8535 24.3445 23.3473 23.7206 22.2368 22.6101C21.1263 21.4996 20.5025 19.9934 20.5025 18.4229C20.5025 16.8524 21.1263 15.3462 22.2368 14.2357C23.3473 13.1252 24.8535 12.5013 26.424 12.5013ZM26.424 7.76408C20.5498 7.76408 15.7652 12.5487 15.7652 18.4229C15.7652 24.2971 20.5498 29.0817 26.424 29.0817C32.2982 29.0817 37.0828 24.2971 37.0828 18.4229C37.0828 12.5487 32.2982 7.76408 26.424 7.76408Z"
+                        fill="white"
+                      />
+                    </svg>
+                    <svg
+                      class="absolute z-0 top-36 left-48 text-white opacity-0 group-hover:opacity-100 group-hover:z-2"
+                      onClick={() => {
+                        handleDownloadImage(url, index);
+                      }}
+                    >
+                      <path
+                        d="M5.3642 38.3726C4.06146 38.3726 2.94663 37.9091 2.0197 36.9822C1.0912 36.0537 0.626953 34.9381 0.626953 33.6354V26.5295H5.3642V33.6354H33.7877V26.5295H38.525V33.6354C38.525 34.9381 38.0615 36.0537 37.1346 36.9822C36.2061 37.9091 35.0904 38.3726 33.7877 38.3726H5.3642ZM19.576 28.8981L7.73283 17.055L11.0489 13.6205L17.2073 19.7789V0.474609H21.9446V19.7789L28.103 13.6205L31.4191 17.055L19.576 28.8981Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </div>
                 </div>
-              </div>
               ))}
               {modalOpen && (
                 <div class="fixed z-10 pt-10  p-2  md:inset-0 backdrop-blur-sm ">
