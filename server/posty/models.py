63a684ba0c17c099083d87fed97d4656d47e05b7
Karan <karanpargal007@gmail.com>
2023-03-14 09:15:13 +0530

    Change models

--- server/posty/models.py
+++ server/posty/models.py
@@ -1,28 +1,10 @@
 from django.db import models
-
-# Create your models here.
 class Template(models.Model):
     template_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
-
-
-class Template_format(models.Model):
-    format_id = models.AutoField(primary_key=True)
     format = models.CharField(max_length=9)
+    S3URL = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.format
-
-
-class Template_color(models.Model):
-    color_id = models.AutoField(primary_key=True)
-    template_id = models.ForeignKey(Template, on_delete=models.CASCADE)
-    format_id = models.ForeignKey(Template_format, on_delete=models.CASCADE)
-    color = models.CharField(max_length=30)
-    templateS3URL = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.color
+        return self.S3URL
+    
+    
